22d8119dff7aaf88339c70d45ab61576f4605a40
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-08 16:13:51 -0500

    sucessfully created routes to find thought by id, update thought and delete thought by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -31,5 +31,52 @@ module.exports = {
           : res.json('Created the thought 🎉')
       )
       .catch((err) => res.status(500).json(err));
-  }
+  },
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select('-__v')
+      .lean()
+      .then(async (thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : res.json({
+              thought,
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  updateThought(req, res) {
+    console.log('You are updating a Thought');
+    console.log(req.body);
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: { thoughtText: req.body.thoughtText, reactions: req.body.reactions } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res
+              .status(404)
+              .json({ message: 'No thought found with that ID :(' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No such thought exists' })
+          : res.json({
+            thought,
+          })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 const {
   getThoughts,
   createThought,
+  getSingleThought,
+  updateThought,
+  deleteThought,
 } = require('../../controllers/thoughtController');
 
 router.route('/').get(getThoughts).post(createThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 module.exports = router;
